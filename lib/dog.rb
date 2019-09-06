962c707603f0821aa91acd46ac2fdd57d2d1d3cf
upbeat-torrent-2014 <ramaabrahams@yahoo.com>
2019-09-06 02:58:08 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+class Dog 
+end 
+
+class Dog
+  # Class body
+ 
+  # Instance Method Definition
+  def bark
+    puts "Woof!"
+  end
+end
+ 
+fido = Dog.new
+fido.bark 
+
+class Dog
+def sit 
+  puts "The Dog is sitting"
+end 
+
+fido = Dog.new
+fido.sit 
+
+end 
